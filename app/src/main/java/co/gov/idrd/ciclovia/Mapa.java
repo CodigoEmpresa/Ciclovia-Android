e6a464aa06231807a289507fedf9490aa4841b5b
Jonathan Castro <jonatancastro1@gmail.com>
2017-11-16 14:38:29 -0500

    Arreglo error fragmet mapas

--- app/src/main/java/co/gov/idrd/ciclovia/Mapa.java
+++ app/src/main/java/co/gov/idrd/ciclovia/Mapa.java
@@ -5,10 +5,14 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.PermissionChecker;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,6 +36,8 @@ public class Mapa extends Fragment {
     private GoogleMap gmap;
     private Context context;
     private Activity activity;
+    private LocationManager locationManager;
+    private int PERMISO_DE_RASTREO = 0;
 
     public Mapa() {
         // Required empty public constructor
@@ -44,7 +50,6 @@ public class Mapa extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_mapa, container, false);
         context = getContext();
         activity = getActivity();
-
         map = (MapView) rootView.findViewById(R.id.map);
         map.onCreate(savedInstanceState);
 
@@ -69,30 +74,11 @@ public class Mapa extends Fragment {
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(center).zoom(12).build();
                 gmap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
-                // For showing a move to my location button
-                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
 
-                gmap.setMyLocationEnabled(true);
             }
         });
 
         return rootView;
-    }
-
-    protected void createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     @Override
@@ -124,5 +110,48 @@ public class Mapa extends Fragment {
     public void onLowMemory() {
         super.onLowMemory();
         map.onLowMemory();
+    }
+
+    private class LocationTracker implements LocationListener {
+        private LocationManager locationManager;
+        private GoogleMap map;
+
+        public LocationTracker(Activity activity, GoogleMap map) {
+            this.locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+            this.map = map;
+            if (ActivityCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_CONTACTS}, PERMISO_DE_RASTREO);
+
+                return;
+            }
+
+            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            else if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+
+            gmap.setMyLocationEnabled(true);
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
     }
 }
